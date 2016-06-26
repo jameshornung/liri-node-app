4a1816a557663256328c7072246c8d67d5cbc184
James Hornung <hornung.james@gmail.com>
2016-06-25 21:30:08 -0500

    formatting to console view

--- liri.js
+++ liri.js
@@ -1,17 +1,18 @@
 //captures the user input on the command line
 var input = process.argv[2];
 
-//variables for spotify
-var spotify = require('spotify');
-var song = '';
-
-//TWITTER==============================================================================
 //variables required for twitter call
 var keys = require('./keys.js');
 var twitter = require('twitter');
 var parameters = {screen_name: 'jamesellis1981', count: 20};
 var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json&q=jamesellis'
 var client = new twitter(keys.twitterKeys);
+
+//variables for spotify
+var spotify = require('spotify');
+var song = '';
+
+//TWITTER==============================================================================
 //if user input is my-tweets this will post the text only from my last 20 tweets
 if(input == 'my-tweets'){
 	client.get('statuses/user_timeline', parameters, function(err, tweets, response){
@@ -28,14 +29,18 @@ else if(input =='spotify-this-song'){
 	}
 	spotify.search({type: 'track', query: song}, function(err,data){
 		if(!err){
+			console.log('--------------------------------------------------------------');
+			console.log('');
 			console.log('Artist: ' + data.tracks.items[0].artists[0].name);
 			console.log('Album Name: ' + data.tracks.items[0].album.name);
 			console.log('Song Name: ' + data.tracks.items[0].name);
 			console.log('Preview Link: ' + data.tracks.items[0].preview_url);
+			console.log('');
+			console.log('---------------------------------------------------------------');
 		}
 	})
 }
 else if(input == 'movie-this'){
 
-},
+}
 
