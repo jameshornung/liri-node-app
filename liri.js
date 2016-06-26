261b12716ac13923b7031731e00417b6a5a9e93a
James Hornung <hornung.james@gmail.com>
2016-06-26 09:38:34 -0500

    Fully functional, no bonus

--- liri.js
+++ liri.js
@@ -16,6 +16,13 @@ var song = '';
 var request = require('request');
 var movie = '';
 
+//variable for fs
+var fs = require('fs');
+
+// if(input == 'do-what-it-says'){
+// 	input = 
+// }
+
 //TWITTER==============================================================================
 //if user input is my-tweets this will post the text only from my last 20 tweets
 if(input == 'my-tweets'){
@@ -76,7 +83,71 @@ else if(input == 'movie-this'){
 			console.log('----------------------------------------------------------------');
 		}
 	});
-	
+}
+//DO WHAT IT SAYS============================================================================
+else if(input == 'do-what-it-says'){
+	fs.readFile('random.txt', 'utf8', function(error, data){
+		var randomArray = data.split(' ');
+		var command = randomArray[0];
+		var media = '';
 
+		for(i=1;i<randomArray.length;i++){
+		media = media + '+' + randomArray[i];
+		};
+		if(command == 'my-tweets'){
+			console.log('');
+			console.log('-----------------------------------------------------------------------');
+			client.get('statuses/user_timeline', parameters, function(err, tweets, response){
+				if(!err){
+					for(i=19;i>=0;i--)
+					console.log('Tweet #' + (20 - i) + ': ' + tweets[i].text);
+				}
+			})
+		}
+		else if(command =='spotify-this-song'){
+			var song = media;
+			
+			if(song == '+'){
+				var song = 'whats+my+age+again';
+			}
+
+			spotify.search({type: 'track', query: song}, function(err,data){
+				if(!err){
+					console.log('--------------------------------------------------------------');
+					console.log('');
+					console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+					console.log('Album Name: ' + data.tracks.items[0].album.name);
+					console.log('Song Name: ' + data.tracks.items[0].name);
+					console.log('Preview Link: ' + data.tracks.items[0].preview_url);
+					console.log('');
+					console.log('---------------------------------------------------------------');
+				}
+			})
+		}
+		else if(command == 'movie-this'){
+			var movie = media;
+
+			request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json', function (error, response, body) {
+
+				if (!error && response.statusCode == 200) {
+					console.log('');
+					console.log('----------------------------------------------------------------');
+					console.log('Title: ' + JSON.parse(body)['Title']);
+					console.log('IMDB Rating: ' + JSON.parse(body)['imdbRating']);
+					console.log('');
+					console.log('Plot: ' + JSON.parse(body)['Plot']);
+					console.log('');
+					console.log('Actors: ' + JSON.parse(body)['Actors']);
+					console.log('');
+					console.log('Year: ' + JSON.parse(body)['Year']);
+					console.log('Runtime: ' + JSON.parse(body)['Runtime']);
+					console.log('Rated: ' + JSON.parse(body)['Rated']);
+					console.log('');
+					console.log('----------------------------------------------------------------');
+				}
+			});
+		}	
+	})
 };
+
 
